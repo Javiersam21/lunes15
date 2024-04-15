6206f11398b745e931007cf13451d8d8cea2c3f1
ALUMNOS <ALUMNOS@P622.AULAS.COM>
2024-04-15 09:08:11 +0200

    Mi primer Commit

---
+++ src/Repositorios/Tuto.java
@@ -0,0 +1,12 @@
+package Repositorios;
+
+public class Tuto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola paco");
+		System.out.println("Y ola a ti tambien lucio");
+	}
+
+}
